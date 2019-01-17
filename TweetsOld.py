750b910f1685ab43978eab5578c834a06b9f8a9a
Alexander Aguilar <aaguila3@villanova.edu>
2019-01-16 22:05:48 -0500

    Give credit when due

--- TweetsOld.py
+++ TweetsOld.py
@@ -3,6 +3,8 @@
 #except I am not able to check if the tweet contains a photo at all, hence I can't return a photo here like I can with the Twitter API.
 #Just wanted to show both sides of the issues I was having
 
+#Code Used: https://github.com/Jefferson-Henrique/GetOldTweets-python
+
 import GetOldTweets3
 
 tweetCriteria = GetOldTweets3.manager.TweetCriteria().setQuerySearch("#dctech").setSince("2018-12-16").setUntil("2019-01-16")
